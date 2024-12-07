d98f82bebde5cb393989f20af5dbe32b61685726
Emanuel Marroquin <carlosema322@gmail.com>
2024-12-07 09:56:08 -0600

    Basic Express server with dynamic port configuration

--- src/index.mjs
+++ src/index.mjs
@@ -2,8 +2,9 @@ import express from 'express';
 
 const app = express();
 
+//use the envPORT or 3000 port
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Runing on Port ${PORT}`);
+    console.log(`Runing on Port ${PORT} !`);
 })
