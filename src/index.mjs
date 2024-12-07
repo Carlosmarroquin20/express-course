b3e88fb9df1339bfe22c9fc9c1a4477147b0c480
Emanuel Marroquin <carlosema322@gmail.com>
2024-12-07 10:34:19 -0600

    Add routes for users, products, and root with JSON responses

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,31 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
 //use the envPORT or 3000 port
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (request, response) => {
+    response.status(201).send({ msg: "Hello"});
+});
+
+//request
+app.get('/api/users', (request, response) => {
+    response.send([
+        { id:1, username: "Ema", displayName: "Ema322"},
+        { id:2, username: "Carlos", displayName: "Carlos2"},
+        { id:3, username: "Carmen", displayName: "Carmencha"}]);
+});
+
+app.get('/api/products', (request, response) => {
+    response.send([{id: 23, productName: "Mouse", prize: 33}]);
+})
+
+app.get('/api/test', (request, response) => {
+
+});
+
+
 app.listen(PORT, () => {
     console.log(`Runing on Port ${PORT} !`);
-})
+});
