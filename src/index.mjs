005b715420e6a37b8550f4d0836b38c024d19f4e
Emanuel Marroquin <carlosema322@gmail.com>
2024-12-09 17:45:56 -0600

    Add dynamic route for users with ID validation and error handling

--- src/index.mjs
+++ src/index.mjs
@@ -5,16 +5,30 @@ const app = express();
 //use the envPORT or 3000 port
 const PORT = process.env.PORT || 3000;
 
+const mockUsers = [
+    { id:1, username: "Ema", displayName: "Ema322"},
+    { id:2, username: "Carlos", displayName: "Carlos2"},
+    { id:3, username: "Carmen", displayName: "Carmencha"}];
+
 app.get("/", (request, response) => {
     response.status(201).send({ msg: "Hello"});
 });
 
 //request
 app.get('/api/users', (request, response) => {
-    response.send([
-        { id:1, username: "Ema", displayName: "Ema322"},
-        { id:2, username: "Carlos", displayName: "Carlos2"},
-        { id:3, username: "Carmen", displayName: "Carmencha"}]);
+    response.send(mockUsers);
+});
+
+
+app.get('/api/users/:id', (request, response) => {
+    console.log(request.params);
+    const parsedId = parseInt(request.params.id);
+    console.log(parsedId);
+    if (isNaN(parsedId)) 
+        return response.status(400).send({msg: "Bad Request, Invalid ID"});
+    const findUser = mockUsers.find((user) => user.id === parsedId);
+    if (!findUser) return response.sendStatus(404);
+    return response.send(findUser);
 });
 
 app.get('/api/products', (request, response) => {
