ec5175081940af2404063a5fd91c6a5ff935dd72
Emanuel Marroquin <carlosema322@gmail.com>
2024-12-13 10:05:09 -0600

    Add PATCH route to partially update users by ID with validation

--- src/index.mjs
+++ src/index.mjs
@@ -65,14 +65,24 @@ app.get('/api/test', (request, response) => {
 //put method
 app.put('/api/users/:id', (request, response) => {
     const { body, params: {id}} = request;
-
     const parsedId = parseInt(id);
     if (isNaN(parsedId)) return response.sendStatus(400);
-
     const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-
-    if (findUserIndex === -1) return response.sendStatus(400);
+    if (findUserIndex === -1) return response.sendStatus(404);
     mockUsers[findUserIndex] = {id: parsedId, ...body  };
+    return response.sendStatus(200);
+});
+
+
+
+//patch method
+app.patch('/api/users/:id', (request, response) => {
+    const { body, params: {id}} = request;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) return response.sendStatus(400);
+    const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+    if (findUserIndex === -1) return response.sendStatus(404);
+    mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body};
     return response.sendStatus(200);
 });
 
